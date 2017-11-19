7738aaed0db85f8f3e29c2d13a9f89424d539f8f
张德满 <2215508028@qq.com>
2017-11-19 20:47:13 +0800

    页面共用部分拆分

---
+++ resources/views/common/footer.blade.php
@@ -0,0 +1,23 @@
+<!-- Javascript Libraries -->
+<!-- jQuery -->
+<script src="/bootstrap/js/jquery.min.js"></script> <!-- jQuery Library -->
+
+<!-- Bootstrap -->
+<script src="/bootstrap/js/bootstrap.min.js"></script>
+
+<!--  Form Related -->
+<script src="/bootstrap/js/icheck.js"></script> <!-- Custom Checkbox + Radio -->
+<script src="/bootstrap/js/autosize.min.js"></script> <!-- Textarea autosize -->
+
+<!-- UX -->
+<script src="/bootstrap/js/scroll.min.js"></script> <!-- Custom Scrollbar -->
+
+<!-- Other -->
+<script src="/bootstrap/js/calendar.min.js"></script> <!-- Calendar -->
+<script src="/bootstrap/js/feeds.min.js"></script> <!-- News Feeds -->
+
+
+<!-- All JS functions -->
+<script src="/bootstrap/js/functions.js"></script>
+</body>
+</html>
---
+++ resources/views/common/header.blade.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<!--[if IE 9 ]>
+<html class="ie9">
+<![endif]-->
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <meta name="format-detection" content="telephone=no">
+    <meta charset="UTF-8">
+
+    <meta name="description" content="Violate Responsive Admin Template">
+    <meta name="keywords" content="Super Admin, Admin, Template, Bootstrap">
+
+    <title>Super Admin Responsive Template</title>
+
+    <!-- CSS -->
+    <link href="/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/bootstrap/css/animate.min.css" rel="stylesheet">
+    <link href="/bootstrap/css/font-awesome.min.css" rel="stylesheet">
+    <link href="/bootstrap/css/form.css" rel="stylesheet">
+    <link href="/bootstrap/css/calendar.css" rel="stylesheet">
+    <link href="/bootstrap/css/style.css" rel="stylesheet">
+    <link href="/bootstrap/css/icons.css" rel="stylesheet">
+    <link href="/bootstrap/css/generics.css" rel="stylesheet">
+</head>
+<body id="skin-blur-violate">
+<header id="header" class="media">
+    <a href="" id="menu-toggle"></a>
+    <a class="logo pull-left" href="index.html">管理系统</a>
+
+    <div class="media-body">
+        <div class="media" id="top-menu">
+            <div class="pull-left tm-icon">
+                <a data-drawer="messages" class="drawer-toggle" href="">
+                    <i class="sa-top-message"></i>
+                    <i class="n-count animated">5</i>
+                    <span>Messages</span>
+                </a>
+            </div>
+            <div class="pull-left tm-icon">
+                <a data-drawer="notifications" class="drawer-toggle" href="">
+                    <i class="sa-top-updates"></i>
+                    <i class="n-count animated">9</i>
+                    <span>Updates</span>
+                </a>
+            </div>
+
+
+
+            <div id="time" class="pull-right">
+                <span id="hours"></span>
+                :
+                <span id="min"></span>
+                :
+                <span id="sec"></span>
+            </div>
+
+            <div class="media-body">
+                <input type="text" class="main-search">
+            </div>
+        </div>
+    </div>
+</header>
+
+<div class="clearfix"></div>
